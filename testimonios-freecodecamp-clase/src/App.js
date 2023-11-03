5c89bb97dba04eb689f911b3e65b94f7fe2e087b
marcos-davi <77940812+marcos-davi@users.noreply.github.com>
2023-11-03 18:19:07 +0100

    final testimonio clase

--- testimonios-freecodecamp-clase/src/App.js
+++ testimonios-freecodecamp-clase/src/App.js
@@ -1,57 +1,37 @@
+import React from 'react';
 import './App.css';
-import { Testimonio } from './componentes/Testimonio.js'
+import Testimonio from './componentes/Testimonio'
 import { users } from './Users.js'
 
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className='contenedor-principal'>
+          <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+          {
+            users.map((user, index) => {
+              return (<Testimonio
+                key={user.id}
+                nombre={user.nombre}
+                pais={user.pais}
+                imagen={user.imagen}
+                alt={user.nombre}
+                cargo={user.cargo}
+                empresa={user.empresa}
+                testimonio={user.testimonio} />);
+            })
+          }
 
-function App() {
-  return (
-    <div className="App">
-      <div className='contenedor-principal'>
-        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        {
-          users.map((user, index) => {
-            return (<Testimonio
-              key ={index}
-              nombre={user.nombre}
-              pais={user.pais}
-              imagen={user.imagen}
-              alt={user.alt}
-              cargo={user.cargo}
-              empresa={user.empresa}
-              testimonio={user.testimonio} />)
-          })
-        }
-
-
-        {/* <Testimonio
-          nombre='Shawn Wang'
-          pais='Singapur'
-          imagen='shawn'
-          alt='Shawn'
-          cargo='Ingeniero de Software'
-          empresa='Amazon'
-          testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.' />
-        <Testimonio
-          nombre='Sarah Chima'
-          pais='Nigeria'
-          imagen='sarah'
-          alt='Sarah'
-          cargo='Ingeniera de Software'
-          empresa='ChatDesk'
-          testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.' />
-        <Testimonio
-          nombre='Emma Bostian'
-          pais='Suecia'
-          imagen='emma'
-          alt='Emma'
-          cargo='Ingeniera de Software'
-          empresa='Spotify'
-          testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.' /> */}
+        </div>
 
       </div>
+    );
 
-    </div>
-  );
+  }
 }
+
+
+
 
 export default App;
--- testimonios-freecodecamp-clase/src/componentes/Testimonio.js
+++ testimonios-freecodecamp-clase/src/componentes/Testimonio.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import '../hojas-de-estilo/Testimonio.css';
 
-export function Testimonio(props) {
-  return (
-    <div className='contenedor-testimonio'>
-      <img className='imagen-testimonio'
-        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
-        alt={`Foto de ${props.alt}`} />
-      <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>
-          <strong>{props.nombre}</strong> en {props.pais}</p>
-        <p className='cargo-testimonio'>
-          {props.cargo} en <strong>{props.empresa}</strong></p>
-        <p className='texto-testimonio'>"{props.testimonio}"</p>
+class Testimonio extends React.Component {
+  render() {
+    return (
+      <div className='contenedor-testimonio'>
+        <img className='imagen-testimonio'
+          src={require(`../imagenes/testimonio-${this.props.imagen}.png`)}
+          alt={`Foto de ${this.props.nombre}`} />
+        <div className='contenedor-texto-testimonio'>
+          <p className='nombre-testimonio'>
+            <strong>{this.props.nombre}</strong> en {this.props.pais}</p>
+          <p className='cargo-testimonio'>
+            {this.props.cargo} en <strong>{this.props.empresa}</strong></p>
+          <p className='texto-testimonio'>"{this.props.testimonio}"</p>
+        </div>
       </div>
-    </div>
+    );
+  }
+} 
 
-  );
-}
+export default Testimonio
 
 
 
